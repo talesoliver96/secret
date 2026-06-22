9e5d817eec94a151b0dfcd47fefaf804227830db
Tales Carneiro <talesoliver96@gmail.com>
2026-06-22 15:18:42 -0300

    fixed

--- frontend/src/pages/customer/MenuPage.jsx
+++ frontend/src/pages/customer/MenuPage.jsx
@@ -64,7 +64,7 @@ export default function MenuPage() {
       }, []);
 
     return [
-      { name: "Burgers", slug: "todos", icon: "🍔" },
+      { name: "Menu", slug: "todos", icon: "🍔" },
       ...unique.map((category) => ({
         ...category,
         icon: getCategoryIcon(category),
